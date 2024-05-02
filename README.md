160a41e3852a77dea647a41be6d72f22d4afcf7f
rohandevtown <rohankinnal@devtown.in>
2024-05-02 19:21:43 +0530

    d2

--- README.md
+++ README.md
@@ -5,5 +5,18 @@
 ## Commands:
 
     >> npm init
+    >> npm i
     >> npm i nodemon
     >> npm run dev
+
+## HTTP Methods:
+
+    >> GET  : Getting the data
+    >> POST :  Sending data to the server/db
+    >> DELETE: Delete data
+    >> PATCH : Update certain fields
+    >> PUT  : Full Update
+
+> > Post Man
+> > Thunder Client
+> > Insomnia
--- server.js
+++ server.js
@@ -2,9 +2,37 @@ const http = require("http"); // package import
 
 const port = 8081; // PORT Num
 
+const toDoList = ["rohan", "rohit", "anil", "anup"];
+
 http.createServer((request, response)=>{
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("<h1>Hello World! - Rohan Kinnal   </h1>");
+    const {method, url} = request;
+    // console.log(method, url);
+
+    if(url === "/todos"){
+        if(method === "GET"){
+            response.writeHead(200);
+            response.write(toDoList.toString())
+        }else if (method === "POST"){
+            let body = "";
+            request.on('error',(err)=>{
+                console.log(err);
+            }).on('data',(chunk)=>{
+                body += chunk
+                console.log("chunk:", chunk)
+            }).on('end',()=>{
+                body = JSON.parse(body)
+                console.log("body: ", body)
+            })
+        }
+        
+        else{
+            response.writeHead(501);
+        }
+    }else if (url === "/"){
+        
+    }
+    // response.writeHead(200, {"Content-Type": "text/html"});
+    // response.write("<h1>Hello World! - Rohan Kinnal   </h1>");
     response.end();
 })
 .listen(port, ()=>{
@@ -12,3 +40,4 @@ http.createServer((request, response)=>{
 })
 
 // http://localhost:8081
+
